a3088fc53e70cd71d8813efc2ef0500ea9692e9d
ishandasofficial <das.ishan71006@gmail.com>
2026-02-19 20:55:29 +0530

    Bhakti Commit

---
+++ Hare_Krishna.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    printf("Hare Krishna Hare Krishna\n");
+    printf("Krishna Krishna Hare Hare\n");
+    printf("Hare Rama Hare Rama\n");
+    printf("Rama Rama Hare Hare\n");
+    return 0;
+}
+
+
+
+
